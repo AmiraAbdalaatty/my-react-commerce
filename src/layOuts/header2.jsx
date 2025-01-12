b781e64a80dcdd31834552ee7c6a3fc5f1eca276
Amira <mustafa@Amira-Macbook-Pro.local>
2025-01-12 14:24:28 +0200

    changed signup to register

--- src/layOuts/header2.jsx
+++ src/layOuts/header2.jsx
@@ -56,7 +56,7 @@ export default function Header2(){
                 onClick={()=>Navigate('/profile')}>Profile</div>}
 
                 {!user ? <div onClick={()=>Navigate('/signup')} >
-                    Sign upppp
+                    register
                 </div> : null}
             </div>
 
